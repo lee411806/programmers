760465cb471eab49bb740f51423764033f10773e
Jaeyong <132039579+lee411806@users.noreply.github.com>
2023-12-26 10:19:26 +0900

    [level unrated] Title: 공백으로 구분하기 1, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        
+        answer = my_string.split(" ");
+        
+        
+        return answer;
+    }
+}
