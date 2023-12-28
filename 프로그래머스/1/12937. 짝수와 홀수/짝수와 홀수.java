fcaefb66b5991ccc7206ad71e156f856c0cce68c
Jaeyong <132039579+lee411806@users.noreply.github.com>
2023-12-28 14:55:45 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+       answer = (num%2==1 || num%2==-1)?  "Odd" : (num%2==0)? "Even": "Unknown" ;
+        return answer;
+    }
+}
