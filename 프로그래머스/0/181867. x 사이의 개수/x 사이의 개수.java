294811618eba067d139127efeaac4423e2eae601
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-05-11 15:39:31 +0900

    [level 0] Title: x 사이의 개수, Time: 2.33 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String myString) {
+        // 문자열을 "x" 기준으로 분할
+        String[] s = myString.split("x",-1);
+        
+        // 결과 배열 생성 (분할된 요소 수만큼)
+        int[] answer = new int[s.length];
+        
+        // 각 분할된 문자열의 길이를 결과 배열에 저장
+        for (int i = 0; i < s.length; i++) {
+            answer[i] = s[i].length();
+        }
+        
+        return answer;
+    }
+}
