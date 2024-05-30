604e362965014bac7edb4bf15f27e6ce63f1b575
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-05-30 16:00:51 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        for(int i=0 ; i < num_list.length ; i++){
+         
+         while(num_list[i] != 1){
+          if(num_list[i] % 2 == 0){
+                
+          }else{
+             num_list[i] -= 1;
+            
+          }
+             num_list[i] /= 2;
+             answer++;
+            
+         }
+        }
+        
+        return answer;
+    }
+}
