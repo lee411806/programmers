4efcff7501e04c04e5fd137dde7b51f45ea73690
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-07-02 12:21:51 +0900

    [level 0] Title: 문자열 묶기, Time: 5.38 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,20 +1,16 @@
+import java.util.*; 
 class Solution {
     public int solution(String[] strArr) {
         
-        int[] count = new int[30]; 
+        int[] iArray = new int[30];
         
-        for(int i=0; i < strArr.length  ;i++){
-            count[strArr[i].length()-1]++; 
+        for(int i =0 ; i < strArr.length ; i++){
+            iArray[strArr[i].length()-1]++;
         }
         
-        int max =0;
-        for(int i=0 ; i<count.length ; i++){
-            if(max < count[i]){
-                max = count[i];
-            }
-        }
+        Arrays.sort(iArray);
         
-        
-        return max;
+        return iArray[29];
+     
     }
 }
