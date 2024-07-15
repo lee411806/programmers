70966185385c4f2714b6a25add9fafa896084439
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-07-15 11:05:41 +0900

    [level 0] Title: 세 개의 구분자, Time: 83.20 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,29 +1,37 @@
 import java.util.*;
 class Solution {
     public List<String> solution(String myStr) {
-        List<String> list = new ArrayList<>();
-        String temp="";    
+                    
+        //2024 07 15 2차 복습 -> 문제 졸업
+        
+        List<String> list = new ArrayList<String>();
+        
+        String temp = "";
+        
+        
         for(int i=0 ; i < myStr.length() ; i++){
-           char c = myStr.charAt(i);
-           if(c == 'a' || c == 'b' || c == 'c'){
-               if(temp != ""){
-                   list.add(temp);
-                   temp="";
-               }
-           }else{
-               temp += c;
-           }
+            char c = myStr.charAt(i);
+            if(c == 'a' || c =='b' || c =='c'){
+                if(temp.equals("")){
+                    
+                }else{
+                    list.add(temp);
+                    temp = "";
+                }
+            }else{
+                 temp += c;
+            }
             
         }
-               if(temp != ""){
-                   list.add(temp);
-                   temp="";
-               }
         
-               if(list.size() == 0){
-                   list.add("EMPTY");
-               } 
-                
+        if(temp != ""){
+        list.add(temp);    
+        }
+        
+        if(list.isEmpty()){
+            list.add("EMPTY");
+        }
+        
         
         return list;
     }
