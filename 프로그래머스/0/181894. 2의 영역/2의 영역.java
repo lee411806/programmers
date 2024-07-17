d2945047fea6fb0a2fc2544d0f951fb2374815a9
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-07-17 13:17:49 +0900

    [level 0] Title: 2의 영역, Time: 1.38 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,36 +1,44 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-       
-        int firstIndex =-1; 
-        int endIndex = -1;
-        boolean twoTrue1 = false;
-        boolean twoTrue2 = false;
-        for(int i=0 ; i<arr.length ; i++){
-           
-            if(arr[i] == 2 ){
-                firstIndex = i;
-                twoTrue1 = true;
-                break;
+        
+            boolean firstTwo = false;
+            boolean secondTwo = false;
+            int firstIndex = -1;
+            int lastIndex = -1;
+        
+        
+            for(int i=0 ; i<arr.length ; i++){
+                
+                if(arr[i] == 2){
+                    firstTwo = true;
+                    firstIndex = i;
+                    break;
+                }
+                
             }
-        }
         
-        for(int i = arr.length-1 ; i>=0 ; i--){
-            if(arr[i] == 2 ){
-                endIndex = i;
-                twoTrue2 = true;
-                break;
+        
+            for(int i=arr.length -1 ; i>= 0 ; i--){
+                
+                if(arr[i] == 2){
+                    secondTwo = true;
+                    lastIndex = i;
+                    break;
+                }
+                
             }
-        }
-        int[] answer;
-        if(twoTrue1 && twoTrue1){
-            answer = Arrays.copyOfRange(arr, firstIndex, endIndex + 1);
-        }else{
-              answer = new int[] {-1};
-        }
+            
+            int[] result;
+                
+            if(firstTwo && secondTwo){
+                return result = Arrays.copyOfRange(arr, firstIndex, lastIndex+1);
+            }else{
+                return result = new int[]{-1};
+            }
         
         
         
-        return answer;
+        
     }
 }
