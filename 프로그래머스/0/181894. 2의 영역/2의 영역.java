c5891981de731d26b22c09dbf4d15100110114ef
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-07-25 14:42:31 +0900

    [level 0] Title: 2의 영역, Time: 1.49 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -2,46 +2,36 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
             
-            
-            //2024 07 17 2차 복습 완
-        
-            boolean firstTwo = false;
-            boolean secondTwo = false;
+            // 20240725 -> 3차복습 사소한 실수 또함!
             int firstIndex = -1;
-            int lastIndex = -1;
+            int endIndex = -1;
+            boolean firstTwo = false;
+            boolean secondTwo = false;    
         
-        
-            for(int i=0 ; i<arr.length ; i++){
-                
-                if(arr[i] == 2){
-                    firstTwo = true;
-                    firstIndex = i;
-                    break;
-                }
-                
+        for(int i=0 ; i < arr.length ; i++){
+            if(arr[i] == 2){
+                firstIndex = i;
+                firstTwo = true;
+                break;
             }
+        }
         
-        
-            for(int i=arr.length -1 ; i>= 0 ; i--){
-                
-                if(arr[i] == 2){
-                    secondTwo = true;
-                    lastIndex = i;
-                    break;
-                }
-                
+        for(int i= arr.length-1 ;  i>=0 ; i--){
+            if(arr[i] == 2){
+                endIndex = i;
+                secondTwo = true;
+                break;
             }
-            
-            int[] result;
-                
-            if(firstTwo && secondTwo){
-                return result = Arrays.copyOfRange(arr, firstIndex, lastIndex+1);
-            }else{
-                return result = new int[]{-1};
-            }
+        }
         
+        int[] result; 
+        if(firstTwo && secondTwo){
+        result = Arrays.copyOfRange(arr, firstIndex, endIndex+1);  
+        }else{
+            result = new int[]{-1};
+        }
         
-        
+        return result;
         
     }
 }
