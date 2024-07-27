fe400026902c53e02c0a614726d7184fb3e827c2
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-07-27 15:10:27 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.38 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,29 +1,35 @@
 class Solution {
     public int[][] solution(int[][] arr) {
-       //20240706 복습 1
+        
+        //2024 07 27 3차 수정
         int row = arr.length;
         int col = arr[0].length;
-
-        if (row > col) {
-            int[][] temp = new int[row][row];
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < col; j++) {
-                    temp[i][j] = arr[i][j];
+        
+        if(row > col){
+            int[][] rectangle = new int[row][row];
+                
+                for(int i=0 ; i < row ; i++){
+                    for(int j=0 ; j < col ; j++){
+                        rectangle[i][j] = arr[i][j];
+                    }
                 }
-                //temp[i][col] = 0;
-            }
-            return temp;
-        } else if (col > row) {
-            int[][] temp = new int[col][col];
-            for (int i = 0; i < col; i++) {
-                for (int j = 0; j < row; j++) {
-                    temp[j][i] = arr[j][i];
+            
+            return rectangle;
+        }else if(col > row){
+             int[][] rectangle = new int[col][col];
+            
+            for(int i=0 ; i < row ; i++){
+                for(int j=0 ; j <col ; j++){
+                    rectangle[i][j] = arr[i][j];
                 }
-               // temp[row][i] = 0;
             }
-            return temp;
-        } else {
+            return rectangle;
+            
+        }else {
             return arr;
         }
+        
+        
+        
     }
 }
