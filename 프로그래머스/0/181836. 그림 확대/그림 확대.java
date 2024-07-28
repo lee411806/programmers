935fc26010e571269288103f353e138b147a65a5
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-07-28 12:06:01 +0900

    [level 0] Title: 그림 확대, Time: 0.34 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -2,33 +2,41 @@ import java.util.*;
 class Solution {
     public List<String> solution(String[] picture, int k) {
             
-        // 20240723 2차 복습 완
-        
+        //2024 07 28 3차 복습
         List<String> list = new ArrayList<>();
-         
-        
-        for(int i=0 ; i < picture.length ;i++){
-          String temp = "";  
-          char[] c = picture[i].toCharArray();
+        StringBuilder sb = new StringBuilder();
+            
+            
+        for(int i=0 ; i < picture.length ; i++){
+            
+            char[] c = picture[i].toCharArray();
             
             for(int j=0 ; j < c.length ; j++){
-               for(int l = 0 ; l< k ; l++){
-                 temp += c[j];    
-               }
-               
-            }
-        
-             for(int m = 0; m < k; m++){
-                 list.add(temp);
             
+                for(int l=0 ; l < k ; l++){
+                    sb.append(c[j]);
+                }  
+                
+               
+             
             }
-        
-        
-        
+            
+            
+            
+            
+         
+                    for(int m=0 ; m<k ; m++){
+                         list.add(sb.toString());
+                    }
+             
+            
+               sb.setLength(0);
+            
         }
         
-        return list; 
         
+        return list;
+            
      
     }
 }
