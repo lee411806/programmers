7c782ec2538b61a5b2a198345e96d60dc18cc542
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-08-01 12:20:21 +0900

    [level 0] Title: 2의 영역, Time: 1.46 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,37 +1,38 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
+        
             
-            // 20240725 -> 3차복습 사소한 실수 또함!
-            int firstIndex = -1;
-            int endIndex = -1;
-            boolean firstTwo = false;
-            boolean secondTwo = false;    
+        int startIndex = -1;
+        int endIndex = -1;
         
-        for(int i=0 ; i < arr.length ; i++){
+        for(int i=0 ; i< arr.length ; i++){
             if(arr[i] == 2){
-                firstIndex = i;
-                firstTwo = true;
+                startIndex = i;
                 break;
             }
         }
         
-        for(int i= arr.length-1 ;  i>=0 ; i--){
-            if(arr[i] == 2){
-                endIndex = i;
-                secondTwo = true;
+        
+        for(int i=arr.length-1 ; i>=0 ; i--){
+            if(arr[i] ==2){
+                endIndex=i;
                 break;
             }
         }
         
-        int[] result; 
-        if(firstTwo && secondTwo){
-        result = Arrays.copyOfRange(arr, firstIndex, endIndex+1);  
-        }else{
+        int[] result;
+        
+        if(startIndex != -1 && endIndex != -1){
+            result = Arrays.copyOfRange(arr, startIndex, endIndex +1);
+        }else {
             result = new int[]{-1};
         }
         
-        return result;
+        
+            return result; 
+        
+        
         
     }
 }
