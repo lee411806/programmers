c8f97475f5687f8e2a05bd31bcfb9861c9462fd8
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-08-01 12:21:23 +0900

    [level 0] Title: 2의 영역, Time: 1.41 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -2,7 +2,11 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         
+          // 20240725 -> 3차복습 사소한 실수 또함!
+        //2024 08 01 3트 
             
+      //     boolean firstTwo = false;
+        //    boolean secondTwo = false;   첫번째, 두번째 2의 유무를 boolean으로 나타낼 수 있다.  
         int startIndex = -1;
         int endIndex = -1;
         
