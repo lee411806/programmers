00ddfec47ebbad8585a33ab1eb0a29e655d965bb
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-09-28 13:24:36 +0900

    [Silver V] Title: 단어 정렬, Time: 340 ms, Memory: 24364 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,4 +1,3 @@
-
 /*
 문제
 N개의 단어 -> 1. 길이가 짧은 것 2. 길이가 같으면 사전 순
@@ -24,6 +23,7 @@ public class Main {
 
         Map<Integer, List<String>> map = new HashMap<>();
 
+        //맵에 단어이 길이와 단어의 길이에 따른 리스트 저장
         for (int i = 0; i < wordCount; i++) {
             String word = br.readLine();
             int wordLength = word.length();
@@ -33,39 +33,39 @@ public class Main {
             map.put(wordLength, wordList); // 리스트를 다시 맵에 저장
         }
 
-        //출력
 
+        // 전부 출력
+        // 중복요소 제거해야 함으로 만약 리스트의 크기가 1보다 클 시 set에 넣었다 빼 줌
+        // 리스트 오름차순 정렬 후 출력
+        int count = 1;
+        while (!map.isEmpty()) {
+            if (map.containsKey(count)) {
+                List<String> wordList = map.get(count);
 
+                if (wordList.size() > 1) {
+                    // Set에 리스트의 모든 요소를 추가 (중복 제거)
+                    Set<String> set = new HashSet<>(wordList);
 
-        int count =1;
-       while(!map.isEmpty()){
-           if(map.containsKey(count)){
-               List<String> wordList = map.get(count);
+                    // Set을 다시 리스트로 변환
+                    wordList = new ArrayList<>(set);
 
-               if (wordList.size() > 1) {
-                   // Set에 리스트의 모든 요소를 추가 (중복 제거)
-                   Set<String> set = new HashSet<>(wordList);
+                    // 리스트를 정렬
+                    Collections.sort(wordList);
+                }
 
-                   // Set을 다시 리스트로 변환
-                   wordList = new ArrayList<>(set);
+                // 리스트의 각 단어를 버퍼 라이터로 출력
+                for (String word : wordList) {
+                    bw.write(word + "\n");
+                }
+            }
 
-                   // 리스트를 정렬
-                   Collections.sort(wordList);
-               }
+            map.remove(count);
+            count++;
 
-               // 리스트의 각 단어를 버퍼 라이터로 출력
-               for (String word : wordList) {
-                   bw.write(word + "\n");
-               }
-           }
+        }
 
-           map.remove(count);
-           count++;
-
-       }
-
-       bw.flush();
-       bw.close();
+        bw.flush();
+        bw.close();
 
     }
 
