1fd0cdd7f9495d84e898879c2b986f273a29640b
Jaeyong <132039579+lee411806@users.noreply.github.com>
2024-10-29 00:22:05 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] map;
+    static boolean[][] visited;
+    static int n, m;
+    static int[] dx = { -1, 1, 0, 0 }; // 상하
+    static int[] dy = { 0, 0, -1, 1 }; // 좌우
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs(0, 0));
+    }
+
+    public static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int nowX = now[0];
+            int nowY = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
+                if (visited[nextX][nextY] || map[nextX][nextY] == 0) continue;
+
+                queue.add(new int[]{nextX, nextY});
+                map[nextX][nextY] = map[nowX][nowY] + 1;
+                visited[nextX][nextY] = true;
+            }
+        }
+
+        return map[n-1][m-1];
+    }
+}
