9c1494f2dba24ff04349c35bbe970c85c353ee28
Jaeyong <132039579+lee411806@users.noreply.github.com>
2025-06-04 14:28:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 38.66 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,30 +1,22 @@
+/*
+문제 : 연속 수 제거
+풀이 : 1. stack에 연속 수 비교하면서 담는다.
+*/
+
+
+
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
+    public Stack<Integer> solution(int []arr) {
         
-        int temp=0;
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<Integer>();
         
+        for(int num : arr){
+            if(stack.isEmpty() || stack.peek() != num )
+                stack.push(num);
+        }
         
-            for(int i=0 ; i<arr.length ;i++){
-                
-                if(stack.isEmpty()  ||  stack.peek() != arr[i]) {
-                    stack.push(arr[i]);
-                   
-                }
-                
-                temp = arr[i];
-                
-
-            }
-        
-                        int[] answer = new int[stack.size()];
-                      
-                                for(int i=stack.size() -1 ; i>=0 ;i--){
-                                     answer[i] = stack.pop();    
-                                }
-        
-        return answer;
+        return stack;
     }
 }
