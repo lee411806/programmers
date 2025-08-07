a9c29882c57e8f8d31e24eff8778eacd79bb9b78
Jaeyong <132039579+lee411806@users.noreply.github.com>
2025-08-07 13:51:22 +0900

    [Silver IV] Title: 수들의 합 2, Time: 144 ms, Memory: 15364 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -0,0 +1,53 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+
+        for(int i=0; i<N ;i++){
+           arr[i] = Integer.parseInt(st1.nextToken());
+        }
+
+        int start =0;
+        int end =0;
+
+        int count =0;
+        int sum = 0;
+
+        while(true){
+
+            if(sum >=M){
+                sum -= arr[start];
+                start ++;
+            }else if(end == N){
+                break;
+            }else{
+                sum += arr[end];
+                end ++;
+            }
+
+            if(sum == M){
+                count++;
+            }
+
+
+        }
+
+        System.out.println(count);
+    }
+}
