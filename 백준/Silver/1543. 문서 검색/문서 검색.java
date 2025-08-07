9276239b5c5db0880c57ef318561a521a9227949
Jaeyong <132039579+lee411806@users.noreply.github.com>
2025-08-07 15:48:48 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,74 +1,39 @@
 
-/*
-문제
-단어가 최대 몇 번 중복되지 않게 등장하는지
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-입력 값
-문서
-검색하고 싶은 단어
-
-풀이
-패턴 매칭 -> KMP 알고리즘
-
- */
-
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        String document = sc.nextLine();
-        String word = sc.nextLine();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int dN = document.length();
-        int wN = word.length();
+        char[] document = br.readLine().toCharArray();
+        char[] search = br.readLine().toCharArray();
 
-        int[] lps = lps(word); // 패턴에 대한 LPS 배열을 구함
+        int count =0;
 
-        int i = 0; // 문서의 인덱스
-        int j = 0; // 패턴의 인덱스
-        int count = 0; // 단어가 등장한 횟수
+        for(int i=0 ; i<= document.length - search.length; i++){
+            int k = i;
 
-        while (i <= dN - wN) { // 더 이상 패턴을 찾을 수 없을 때까지
-            if (document.startsWith(word, i)) {
-                // 패턴이 문서의 i 위치에서 시작된다면
-                count++; // 카운트를 증가시키고
-                i += wN; // 중복을 피하기 위해 단어 길이만큼 건너뜀
-            } else {
-                i++; // 일치하지 않으면 한 칸씩 이동
+            for(int j=0 ; j< search.length; j++){
+
+                if(document[k] == search[j]){
+                    k++;
+                } else{
+                    break;
+                }
+                if(j == search.length-1){
+                    count++;
+                    i += search.length-1;
+                }
             }
+
         }
 
         System.out.println(count);
-    }
 
-    // LPS 배열을 구하는 함수
-    public static int[] lps(String word) {
-        int wordLen = word.length();
-        int[] lps = new int[wordLen];
-        int len = 0;
-        int i = 1;
 
-        lps[0] = 0; // 첫 번째 문자의 lps는 항상 0
-
-        while (i < wordLen) {
-            if (word.charAt(i) == word.charAt(len)) {
-                len++;
-                lps[i] = len;
-                i++;
-            } else {
-                if (len != 0) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-        return lps;
     }
 }
-
-
-
