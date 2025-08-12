dc07f9b1347db995cb9b88f09eb1ee0c505b8d3c
Jaeyong <132039579+lee411806@users.noreply.github.com>
2025-08-12 16:01:44 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,34 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+//        int a = Integer.parseInt(st.nextToken());
+//        int b = Integer.parseInt(st.nextToken());
+
+
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+
+
+
+    }
+}
